3e0b834a681f9106c6aa1c94c598355b29c0ba0c
edneyRoldao <edneyroldao@gmail.com>
2022-09-28 13:32:23 -0300

    pega

--- src/main/java/com/banksystem/banksystem/test/Test.java
+++ src/main/java/com/banksystem/banksystem/test/Test.java
@@ -1,6 +1,7 @@
 package com.banksystem.banksystem.test;
 
 import java.lang.reflect.Method;
+import java.util.Objects;
 
 
 public class Test {
@@ -18,4 +19,35 @@ public class Test {
         }
     }
 
+    static String completeField(String value, int size, Direction direction, CompletionType completionType) {
+        StringBuilder sb = new StringBuilder(Objects.nonNull(value) ? value : "");
+
+        if (sb.length() >= size) {
+            return sb.substring(0, size);
+        }
+
+        String completionValue = completionType == CompletionType.WHITESPACE ? " " : "0";
+
+        while (sb.length() < size)  {
+            switch (direction) {
+                case LEFT:
+                    sb.append(completionValue);
+                    break;
+                case RIGHT:
+                    sb.insert(0, completionValue);
+            }
+        }
+
+        return sb.toString();
+    }
+
+
+}
+
+enum Direction {
+    LEFT, RIGHT
+}
+
+enum CompletionType {
+    ZERO, WHITESPACE
 }
